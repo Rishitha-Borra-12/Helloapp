2b2f7c0d729488365427dc97a696884c7b0a115f
Rishitha-Borra-12 <rb2409@srmist.edu.in>
2026-04-08 23:43:06 +0530

    UC5

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,11 +1,21 @@
+import java.awt.*;
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + " ! ");
+        StringBuilder namesBuilder = new StringBuilder();
 
+        if (args.length == 0) {
+            namesBuilder.append("World");
+        } else {
+            // UC5: Using the Enhanced For Loop (for-each)
+            for (String name : args) {
+
+                if (namesBuilder.length() > 0) {
+                    namesBuilder.append(", ");
+                }
+                namesBuilder.append(name);
+            }
         }
-        else
-        System.out.println("Hello world " );
+
+        System.out.println("Hello, " + namesBuilder.toString() + "!");
     }
 }
